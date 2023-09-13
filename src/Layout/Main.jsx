2015466874b1a3b89f838b610a5bcd3d11423226
SumiAkter2 <akteralisha36@gmail.com>
2023-09-13 11:49:39 +0600

    added Navbar and Footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
-
+import Footer from "../Pages/Shared/Footer";
+import Navbar from "../Pages/Shared/Navbar";
 
 const Main = () => {
-    return (
-        <div>
-           <Outlet></Outlet> 
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
---
+++ src/Pages/Shared/Footer.jsx
@@ -0,0 +1,40 @@
+const Footer = () => {
+  return (
+    <div className="bg-neutral text-neutral-content">
+      <footer className="footer p-10 ">
+        <nav>
+          <header className="footer-title">Services</header>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <header className="footer-title">Company</header>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <header className="footer-title">Legal</header>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+      </footer>
+      <div>
+        <div className=" footer-center p-4">
+          <aside>
+            <p>
+              Copyright © {new Date().getFullYear()} - All right reserved by
+              ACME Industries Ltd
+            </p>
+          </aside>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
